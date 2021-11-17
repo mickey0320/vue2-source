6128a17b5fcd3b192afddeb1c2846ebc99ac2b97
yanjian <yanjian92@126.com>
2021-11-17 13:16:43 +0800

    feat: 完成$mount

--- src/init.js
+++ src/init.js
@@ -1,9 +1,20 @@
 import initState from './state'
+import { compileToFunction } from './compile'
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
     const vm = this
     vm.$options = options
     initState(vm)
+  }
+  Vue.prototype.$mount = function (el) {
+    el = document.querySelector(el)
+    const options = this.$options
+    if (!options.render) {
+      if (!options.template) {
+        options.template = el.outerHTML
+      }
+      options.render = compileToFunction(options.template)
+    }
   }
 }
